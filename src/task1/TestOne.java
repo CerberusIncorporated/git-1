8ee402b1be0aa15ec954c1e3222141ac699ae989
cerberusincorporated <sancez.rick@gmail.com>
2020-02-17 13:49:43 +0200

    Test

---
+++ src/task1/TestOne.java
@@ -0,0 +1,36 @@
+package task1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class TestOne {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Integer> odd = new ArrayList<>();
+        ArrayList<Integer> even = new ArrayList<>();
+        for(int i = 0; i<5;i++){
+
+            int x = Integer.parseInt(reader.readLine());
+            if(x % 2 == 0){
+                even.add(x);
+            }
+            else{
+                odd.add(x);
+            }
+        }
+        System.out.print("Odd: ");
+        for(Integer s:odd){
+            System.out.print(s+" ");
+        }
+        System.out.println(" ");
+        System.out.print("Even: ");
+        for(Integer s:even){
+            System.out.print(s + " ");
+        }
+
+    }
+
+}
